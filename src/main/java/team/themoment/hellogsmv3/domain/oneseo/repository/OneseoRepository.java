d3436fd0f944d4f388b87bea07ae91e49bd1b15c
snowykte0426 <snowykte0426@naver.com>
2025-06-17 18:01:14 +0900

    feat(test-result): 이름과 생년월일을 이용한 원서 조회 메서드 추가

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/repository/OneseoRepository.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/repository/OneseoRepository.java
@@ -7,10 +7,13 @@ import team.themoment.hellogsmv3.domain.oneseo.entity.Oneseo;
 import team.themoment.hellogsmv3.domain.oneseo.repository.custom.CustomOneseoRepository;
 import team.themoment.hellogsmv3.domain.oneseo.entity.type.Screening;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface OneseoRepository extends JpaRepository<Oneseo, Long>, CustomOneseoRepository {
     boolean existsByMember(Member member);
     Optional<Oneseo> findByMember(Member member);
+    @Query("SELECT o FROM Oneseo o JOIN FETCH o.member WHERE o.member.name = :memberName AND o.member.birth = :memberBirth")
+    Optional<Oneseo> findByMemberNameAndMemberBirth(String memberName, LocalDate memberBirth);
 }
