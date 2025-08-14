15552d30331dead4875920f0b106e758461e6089
snowykte0426 <snowykte0426@naver.com>
2025-08-14 11:25:52 +0900

    chore(auth): 중복된 설정을 하던 Session 만료시간 설정 클래스 제거

--- src/main/java/team/themoment/hellogsmv3/global/config/SessionConfig.java
+++
@@ -1,9 +0,0 @@
-package team.themoment.hellogsmv3.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
-public class SessionConfig {
-}
