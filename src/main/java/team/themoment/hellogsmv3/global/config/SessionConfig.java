36547c0b8c04e8694a26f9ddcb14241f5aab8c6a
snowykte0426 <snowykte0426@naver.com>
2025-08-14 11:23:13 +0900

    add(auth): Session 만료시간 설정 클래스 추가

---
+++ src/main/java/team/themoment/hellogsmv3/global/config/SessionConfig.java
@@ -0,0 +1,9 @@
+package team.themoment.hellogsmv3.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
+public class SessionConfig {
+}
