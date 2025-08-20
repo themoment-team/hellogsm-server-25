d832ed55c951832788b3226220da7160c6a7a163
snowykte0426 <snowykte0426@naver.com>
2025-08-20 17:37:24 +0900

    fix(member): 인증 코드 삭제 쿼리 메서드 제거

--- src/main/java/team/themoment/hellogsmv3/domain/member/repository/CodeRepository.java
+++ src/main/java/team/themoment/hellogsmv3/domain/member/repository/CodeRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 public interface CodeRepository extends CrudRepository<AuthenticationCode, String> {
     Optional<AuthenticationCode> findByMemberIdAndAuthCodeType(Long memberId, AuthCodeType authCodeType);
     Optional<AuthenticationCode> findByCode(String code);
-    void deleteByMemberId(Long memberId);
 }
