7a20bf10ec2137be27a46c37a72f7cf0ca1e0848
snowykte0426 <snowykte0426@naver.com>
2025-08-20 16:05:00 +0900

    add(utility): 원서 삭제 API 구현

--- src/main/java/team/themoment/hellogsmv3/domain/common/utility/controller/UtilityController.java
+++ src/main/java/team/themoment/hellogsmv3/domain/common/utility/controller/UtilityController.java
@@ -1,23 +1,27 @@
 package team.themoment.hellogsmv3.domain.common.utility.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import team.themoment.hellogsmv3.domain.common.utility.service.DeleteOneseoService;
 import team.themoment.hellogsmv3.global.common.response.CommonApiResponse;
 
 @RestController
-@RequestMapping
+@RequestMapping("/utility/v3")
 @Tag(name = "Utility API", description = "개발용 유틸리티 API입니다.")
 @RequiredArgsConstructor
 public class UtilityController {
 
+    private final DeleteOneseoService deleteOneseoService;
 
     @Operation(summary = "원서 삭제", description = "입력된 접수 번호에 해당하는 원서를 삭제합니다.")
+    @DeleteMapping("/oneseo")
     public CommonApiResponse deleteOneseo(@RequestParam String submitCode) {
+        deleteOneseoService.execute(submitCode);
         return CommonApiResponse.success(
                 "입력된 접수 번호에 해당하는 원서를 삭제했습니다. 접수 번호: " + submitCode
         );
