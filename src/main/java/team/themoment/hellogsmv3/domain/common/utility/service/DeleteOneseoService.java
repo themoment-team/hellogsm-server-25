331b4047f7375f856a2c818337eea3113d624875
snowykte0426 <snowykte0426@naver.com>
2025-08-20 16:14:41 +0900

    update(utility): 원서 삭제 서비스 클래스에 트랜젝션 선언 추가

--- src/main/java/team/themoment/hellogsmv3/domain/common/utility/service/DeleteOneseoService.java
+++ src/main/java/team/themoment/hellogsmv3/domain/common/utility/service/DeleteOneseoService.java
@@ -3,6 +3,7 @@ package team.themoment.hellogsmv3.domain.common.utility.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import team.themoment.hellogsmv3.domain.oneseo.repository.OneseoRepository;
 import team.themoment.hellogsmv3.global.exception.error.ExpectedException;
 
@@ -12,6 +13,7 @@ public class DeleteOneseoService {
 
     private final OneseoRepository oneseoRepository;
 
+    @Transactional(rollbackFor = Exception.class)
     public void execute(String submitCode) {
         if (oneseoRepository.deleteByOneseoSubmitCode(submitCode)) {
             throw new ExpectedException("해당 접수 번호에 해당하는 원서가 존재하지 않습니다.", HttpStatus.NOT_FOUND);
