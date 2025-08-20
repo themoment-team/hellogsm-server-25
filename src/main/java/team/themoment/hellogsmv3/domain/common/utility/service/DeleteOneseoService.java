8c93f44b91c709f38aa0840ad1a2ea8ada28718a
snowykte0426 <snowykte0426@naver.com>
2025-08-20 17:08:36 +0900

    update(oneseo): 원서 삭제 시 캐싱 삭제 개선

--- src/main/java/team/themoment/hellogsmv3/domain/common/utility/service/DeleteOneseoService.java
+++ src/main/java/team/themoment/hellogsmv3/domain/common/utility/service/DeleteOneseoService.java
@@ -17,15 +17,11 @@ public class DeleteOneseoService {
     private final OneseoRepository oneseoRepository;
 
     @Transactional(rollbackFor = Exception.class)
-    public void execute(String submitCode) {
+    @CacheEvict(value = ONESEO_CACHE_VALUE, key = "#result")
+    public Long execute(String submitCode) {
         Long memberId = oneseoRepository.findMemberIdByOneseoSubmitCode(submitCode)
                 .orElseThrow(() -> new ExpectedException("해당 접수 번호에 해당하는 원서가 존재하지 않습니다.", HttpStatus.NOT_FOUND));
         oneseoRepository.deleteByOneseoSubmitCode(submitCode);
-        evictOneseoCache(memberId);
-    }
-
-    // Redis 캐시에서 해당 memberId에 대한 원서 정보를 제거하기 위한 메서드입니다
-    @CacheEvict(value = ONESEO_CACHE_VALUE, key = "#memberId")
-    public void evictOneseoCache(Long memberId) {
+        return memberId;
     }
 }
