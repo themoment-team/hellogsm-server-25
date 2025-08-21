7c7b484c20c4d25728effaae37d02eb140d6ed47
snowykte0426 <snowykte0426@naver.com>
2025-08-21 20:53:41 +0900

    add:(utility): 사용자 권한 수정 서비스 클래스 추가

---
+++ src/main/java/team/themoment/hellogsmv3/domain/common/utility/service/ModifyMemberRoleService.java
@@ -0,0 +1,35 @@
+package team.themoment.hellogsmv3.domain.common.utility.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.themoment.hellogsmv3.domain.member.entity.Member;
+import team.themoment.hellogsmv3.domain.member.entity.type.Role;
+import team.themoment.hellogsmv3.domain.member.repository.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
+@Profile("!prod")
+public class ModifyMemberRoleService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void execute(String phoneNumber, Role role) {
+        memberRepository.findByPhoneNumber(phoneNumber)
+                .ifPresent(member -> {
+                    Member.builder()
+                            .id(member.getId())
+                            .name(member.getName())
+                            .phoneNumber(member.getPhoneNumber())
+                            .email(member.getEmail())
+                            .role(role)
+                            .sex(member.getSex())
+                            .birth(member.getBirth())
+                            .authReferrerType(member.getAuthReferrerType())
+                            .build();
+                    memberRepository.save(member);
+                });
+    }
+}
