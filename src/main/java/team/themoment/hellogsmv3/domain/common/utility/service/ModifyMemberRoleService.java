cca7d9173323997b2172083fd15456d6de2e5eb8
snowykte0426 <snowykte0426@naver.com>
2025-08-21 22:56:04 +0900

    update(utility): 사용자 권한 수정 서비스에서 사용자 미존재 시 예외 발행 추가

--- src/main/java/team/themoment/hellogsmv3/domain/common/utility/service/ModifyMemberRoleService.java
+++ src/main/java/team/themoment/hellogsmv3/domain/common/utility/service/ModifyMemberRoleService.java
@@ -2,10 +2,13 @@ package team.themoment.hellogsmv3.domain.common.utility.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.themoment.hellogsmv3.domain.member.entity.Member;
 import team.themoment.hellogsmv3.domain.member.entity.type.Role;
 import team.themoment.hellogsmv3.domain.member.repository.MemberRepository;
+import team.themoment.hellogsmv3.global.exception.error.ExpectedException;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +19,8 @@ public class ModifyMemberRoleService {
 
     @Transactional
     public void execute(String phoneNumber, Role role) {
-        memberRepository.findByPhoneNumber(phoneNumber)
-                .ifPresent(member -> member.modifyMemberRole(role));
+        Member member = memberRepository.findByPhoneNumber(phoneNumber)
+                .orElseThrow(() -> new ExpectedException("해당 전화 번호에 해당하는 계정이 존재하지 않습니다.", HttpStatus.NOT_FOUND));
+        member.modifyMemberRole(role);
     }
 }
