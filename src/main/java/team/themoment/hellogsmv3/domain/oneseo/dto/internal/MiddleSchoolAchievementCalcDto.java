a6b7fcc0a68129a167c33168b81aa6f3f8024b2e
snowykte0426 <snowykte0426@naver.com>
2025-09-21 22:34:39 +0900

    update(oneseo): 유효하지 않은 JavaDoc 주석 위치 변경

--- src/main/java/team/themoment/hellogsmv3/domain/oneseo/dto/internal/MiddleSchoolAchievementCalcDto.java
+++ src/main/java/team/themoment/hellogsmv3/domain/oneseo/dto/internal/MiddleSchoolAchievementCalcDto.java
@@ -5,12 +5,12 @@ import lombok.Builder;
 import java.math.BigDecimal;
 import java.util.List;
 
-@Builder
 /**
  * 중학교 성적 계산용 DTO
  * 성적이 복사된 상태의 DTO입니다.
  * 과목의 이름을 나타내는 generalSubjects,newSubjects,artsPhysicalSubjects가 없습니다.
  */
+@Builder
 public record MiddleSchoolAchievementCalcDto(
         List<Integer> achievement1_2,
         List<Integer> achievement2_1,
