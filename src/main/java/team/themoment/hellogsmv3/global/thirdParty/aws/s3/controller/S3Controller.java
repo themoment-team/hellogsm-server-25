edd8cab849b16ba3d93e0e29c2249a1997e9aa80
snowykte0426 <snowykte0426@naver.com>
2025-09-21 22:58:00 +0900

    update(thirdParty): S3 컨트롤러에서 미사용 종속성 제거

--- src/main/java/team/themoment/hellogsmv3/global/thirdParty/aws/s3/controller/S3Controller.java
+++ src/main/java/team/themoment/hellogsmv3/global/thirdParty/aws/s3/controller/S3Controller.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import team.themoment.hellogsmv3.global.common.response.CommonApiResponse;
 import team.themoment.hellogsmv3.global.thirdParty.aws.s3.dto.response.UploadImageResDto;
 import team.themoment.hellogsmv3.global.thirdParty.aws.s3.service.UploadImageService;
 
